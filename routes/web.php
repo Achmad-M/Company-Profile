c383abe655c96e637441c6e90100323e80cecd39
Achmad-M <achmadmaulanakusnadi@gmail.com>
2023-11-09 13:40:39 +0800

    mengatur routes dan blade

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,42 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/classes', function () {
+    return view('classes');
+})->name('classes');
+
+Route::get('/facility', function () {
+    return view('facility');
+})->name('facility');
+
+Route::get('/team', function () {
+    return view('team');
+})->name('team');
+
+Route::get('/call-to-action', function () {
+    return view('call-to-action');
+})->name('call-to-action');
+
+Route::get('/appointment', function () {
+    return view('appointment');
+})->name('appointment');
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+})->name('testimonial');
+
+Route::get('/error', function () {
+    return view('404');
+})->name('error');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+?>
